0b7a4a70e9b2929d50dbf2ea629a356bf800db90
Fenerbahce19077 <93336389+Fenerbahce19077@users.noreply.github.com>
2021-11-04 18:37:13 +0100

    Add files via upload

--- js/recVideo.js
+++ js/recVideo.js
@@ -54,3 +54,22 @@ function getUserMedia(options, successCallback, failureCallback) {
       })
       .catch(err => alert('Error: ' + err));
   }
+
+const saveImage = function(){
+    rawImage = document.getElementById('imageTag');
+    imgData = getBase64Image(bannerImage);
+    localStorage.setItem("imgData", imgData);
+};
+
+function getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+};
